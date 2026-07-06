2fa4ea3cc0714aabf74ed1a8ee16895cb1b33e48
Kaloyan19 <teodosievkaloyan@gmail.com>
2026-07-06 19:03:23 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/minibilling/MinibillingApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minibilling/MinibillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
