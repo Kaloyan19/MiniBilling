eb161f3adfe8e78eef95ee656fb3611ed0b48455
Kaloyan19 <teodosievkaloyan@gmail.com>
2026-07-22 16:10:19 +0300

    removed LineENtityInterfacce its not needed

--- src/main/java/com/example/minibilling/repository/jpa/LineEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.minibilling.repository.jpa;
-
-import com.example.minibilling.model.entity.LineEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LineEntityRepository extends JpaRepository<LineEntity, String>{
-}
